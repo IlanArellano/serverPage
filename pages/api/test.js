cf6a170929c9aca7ad2a237abb50d24077fffe7a
Ilan Haziel Arellano Zavala <ilanarellano15@gmail.com>
2021-09-07 19:53:01 -0500

    Terminando el paso 1 para registrarse

--- pages/api/test.js
+++ pages/api/test.js
@@ -2,13 +2,10 @@ import { getConnection } from "../../db";
 
 export default async function test(req, res) {
   try {
-    res.status(201).json({
-      server: process.env.DB_SERVER,
-      usuario: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      port: process.env.DB_PORT,
-      database: process.env.DB_DATABASE,
-    });
+    const pool = await getConnection();
+    const query = await pool.request().query("SELECT * FROM rangos");
+
+    res.status(200).json(query);
   } catch (error) {
     res.status(500).json({ error });
   }
